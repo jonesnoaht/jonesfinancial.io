8cfd747799557dc6045b7b05c71a036ffd50604e
Noah Jones <noahjones@Brittanys-M1-MacBook-Air.local>
2021-10-25 17:29:27 -0400

    I refactored the pages so that I could just change a .js file and every other page would be updated.

---
+++ head-2.js
@@ -0,0 +1,9 @@
+document.write('\
+    <div class="head" style="align-items: center; position: relative; margin-right: auto; margin-left: auto; width: 310px;">\
+        <img src="https://ipfs.io/ipfs/QmVXGDUs16Rs48S8S2fnprdrKhwhq2MuYECnqUP4T\
+9Y1Hn?filename=torus.svg" style="height: 50px; position: float; float: left; z-index: 0; margin-top: 25px;">\
+        <div class=“header” style="color:rgb(22, 66, 196); position: float; float: left; align: left; z-index: 1; margin-left: 10px; margin-right: auto;">  \
+		    <h1 style="font: 40px serif; font-family: "Forum", serif;">Jones Financial</h1>\
+        </div>\
+    </div>\
+')
---
+++ navbar.js
@@ -0,0 +1,12 @@
+document.write('\
+<div class="navbar" style="font-size: 14px; background: rgb(77, 77, 77); overflow: hidden; padding: 5px; margin-top: 100px;">\
+	<a href="#" \
+style="text-decoration:none;float:left;color:rgb(255,255,255);padding:5px;font-size:20px;"><b>Home</b></a>\
+	<a href="http://www.jonesfinancial.io" \
+style="text-decoration:none;float:left;color:rgb(255,255,255);padding:5px;font-size:20px;">About</a>\
+	<a href="http://www.jonesfinancial.io/about" \
+style="text-decoration:none;float:left;color:rgb(255,255,255);padding:5px;font-size:20px;">Services</a>\
+	<a href="https://www.twitter.com/notmyscienceac1" \
+style="text-decoration:none;float:right;color:rgb(255,255,255);padding:5px;font-size:20px;">Contact Us</a>\
+</div>\
+')
