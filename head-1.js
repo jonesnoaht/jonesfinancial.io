d05f6c67171f3ec5f51f2e4a461f76abef21bed6
Noah Jones <noahjones@Brittanys-M1-MacBook-Air.local>
2021-10-26 19:00:23 -0400

    hello

---
+++ head-1.js
@@ -0,0 +1,9 @@
+document.write('\
+    <div class="head" style="align-items: center; justify-content: center; display: flex; position: absolute;">\
+        <img src="https://ipfs.io/ipfs/QmVXGDUs16Rs48S8S2fnprdrKhwhq2MuYECnqUP4T9Y1Hn?filename=torus.svg" style="height: 500px; position: absolute; margin-left: auto; margin-right: auto; z-index: 0; ">\
+               <div class=“header” style="color:rgb(22, 66, 196);\
+	text-align:center; z-index: 1; ">  \
+	       <h1 style="font: 220px serif; font-family: "Forum", serif;">Jones Financial</h1>\
+               </div>\
+	       </div>\
+	       ')
