38552464bbd8d066dd12b3564a33c538964884d1
Noah Jones <noahjones@Brittanys-M1-MacBook-Air.local>
2021-10-26 20:37:58 -0400

    updated links and such

--- footer.js
+++ footer.js
@@ -2,11 +2,11 @@ document.write('\
 <footer style="background-color: lightblue; width: 100%; padding: 2%;">\
 <table style="width: 100%; align: center;">\
     <tbody style="color: rgb(255, 255, 255);">\
-        <tr><th style="width: 25%;"><a href="" style="text-decoration: none; color: rgb(255, 255, 255);"> Main</th><th style="font-weight: bolder;">Projects</th><th style="font-weight: bolder;">Affiliates</th></tr>\
-        <tr><td style="width: 25%;"><a href="" style="text-decoration: none; color: rgb(255, 255, 255);">About</a></td><td style="width: 25%;">CD Token</td><td style="width: 25%;">IOG</td></tr>\
-        <tr><td style="width: 25%;"><a href="" style="text-decoration: none; color: rgb(255, 255, 255);">Services</a></td><td style="width: 25%;">NFT</td><td style="width: 25%;">Catalyst</td></tr>\
-        <tr><td style="width: 25%;"><a href="" style="text-decoration: none; color: rgb(255, 255, 255);">Contact Us</a></td><td style="width: 25%;">Education</td><td style="width: 25%;">We Are Cardano</td></tr>\
-        <tr><td style="width: 25%;"><a href="" style="text-decoration: none; color: rgb(255, 255, 255);">Ethics Statement</a></td><td style="width: 25%;"></td><td style="width: 25%;">Cardano Acropolis</td></tr>\
+        <tr><th style="width: 25%;"><a href="http://www.jonesnoaht.io/" style="text-decoration: none; color: rgb(255, 255, 255);"> Main</th><th style="font-weight: bolder;">Projects</th><th style="font-weight: bolder;">Affiliates</th></tr>\
+        <tr><td style="width: 25%;"><a href="http://www.jonesnoaht.io/about" style="text-decoration: none; color: rgb(255, 255, 255);">About</a></td><td style="width: 25%;"><a href="http://www.jonesnoaht.io/cdt">CD Token</a></td><td style="width: 25%;">IOG</td></tr>\
+        <tr><td style="width: 25%;"><a href="http://www.jonesnoaht.io/nft" style="text-decoration: none; color: rgb(255, 255, 255);">Services</a></td><td style="width: 25%;">NFT</td><td style="width: 25%;">Catalyst</td></tr>\
+        <tr><td style="width: 25%;"><a href="http://www.jonesnoaht.io/contact" style="text-decoration: none; color: rgb(255, 255, 255);">Contact Us</a></td><td style="width: 25%;">Education</td><td style="width: 25%;">We Are Cardano</td></tr>\
+        <tr><td style="width: 25%;"><a href="http://www.jonesnoaht.io/ethics" style="text-decoration: none; color: rgb(255, 255, 255);">Ethics Statement</a></td><td style="width: 25%;"></td><td style="width: 25%;"><a href="https://discord.gg/Qp35CJPA">Cardano Acropolis</a></td></tr>\
     </tbody>\
 </table>\
 </footer>\
